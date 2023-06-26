7b0c14c88ebfebb1b5a07619195d26c0b29af845
Dmitry <deslink123@gmail.com>
2023-06-26 18:32:17 +0300

    Управляемые компоненты

--- src/App.js
+++ src/App.js
@@ -14,12 +14,12 @@ function App() {
 		{ id: 3, title: 'Javascript 3', body: 'Description' }
 	])
 
-	const [title, setTitle] = useState('')
-	const bodyInputRef = useRef()
+	const [post, setPost] = useState({ title: '', body: '' })
+
 	const addNewPost = (e) => {
 		e.preventDefault()
-		console.log(title)
-		console.log(bodyInputRef.current.value)
+		setPosts([...posts, { ...post, id: Date.now() }])
+		setPost({ title: '', body: '' })
 	}
 
 	return (
@@ -27,16 +27,17 @@ function App() {
 			<form>
 				{/*Управляемый компонент*/}
 				<MyInput
-					value={title}
-					onChange={e => setTitle(e.target.value)}
+					value={post.title}
+					onChange={e => setPost({ ...post, title: e.target.value })}
 					type="text"
 					placeholder='Название поста'
 				/>
 				{/*Неуправляемый компонент*/}
-				<MyInput 
-					ref={bodyInputRef}
-					type="text" 
-					placeholder='Описание поста' 
+				<MyInput
+					value={post.body}
+					onChange={e => setPost({ ...post, body: e.target.value })}
+					type="text"
+					placeholder='Описание поста'
 				/>
 				<MyButton onClick={addNewPost}>Создать пост</MyButton>
 			</form>
