1a65d03604c085236777655a047d5ad22e451f07
Dmitry <deslink123@gmail.com>
2023-07-03 19:18:13 +0300

    Компонент загрузки (крутилка)

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo, useEffect } from 'react';
 import '../src/styles/App.css'
 import PostList from './components/PostList';
 import PostForm from './components/PostForm';
@@ -6,17 +6,34 @@ import PostFilter from './components/PostFilter';
 import MyModal from './components/UI/MyModal/MyModal';
 import MyButton from './components/UI/button/MyButton';
 import { usePosts } from './hooks/usePosts';
+import axios from 'axios';
+import PostService from './API/PostService';
+import Loader from './components/UI/Loader/Loader';
 
 function App() {
 	const [posts, setPosts] = useState([])
 	const [filter, setFilter] = useState({ sort: '', query: '' })
 	const [modal, setModal] = useState(false)
-
 	const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
+	const [isPostsLoading, setIsPostsLoading] = useState(false)
+
+	useEffect(() => {
+		fetchPosts()
+	}, [])
 
 	const createPost = (newPost) => {
 		setPosts([...posts, newPost])
 		setModal(false)
+	}
+
+	async function fetchPosts() {
+		setIsPostsLoading(true)
+		setTimeout(async () => {
+
+			const posts = await PostService.getAll()
+			setPosts(posts)
+			setIsPostsLoading(false)
+		}, 1000)
 	}
 
 	const removePost = (post) => {
@@ -36,7 +53,10 @@ function App() {
 				filter={filter}
 				setFilter={setFilter}
 			/>
-			<PostList remove={removePost} posts={sortedAndSearchedPosts} title="Список постов 1" />
+			{isPostsLoading
+				? <div style={{ display: 'flex', justifyContent: 'center', marginTop: '50px' }}><Loader /></div>
+				: <PostList remove={removePost} posts={sortedAndSearchedPosts} title="Список постов" />
+			}
 		</div>
 	);
 }
