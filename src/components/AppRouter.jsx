57ea0117b0eca89029876f368bdcbad0249d0162
Dmitry <deslink123@gmail.com>
2023-07-05 20:10:33 +0300

    Логин

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -4,16 +4,38 @@ import About from '../pages/About'
 import Posts from '../pages/Posts'
 import Error from '../pages/Error'
 import PostIdPage from '../pages/PostIdPage'
+import { privateRoutes, publicRoutes } from '../router/Routes'
 
 const AppRouter = () => {
+    const isAuth = false;
     return (
-        <Routes>
-            <Route path="/about" Component={About} />
-            <Route path="/posts" Component={Posts} />
-            <Route exact path="/posts/:id" Component={PostIdPage}/>
-            <Route path="/error" Component={Error} />
-            <Route path='/*' element={<Navigate to="/error" />} />
-        </Routes>
+        isAuth
+            ?
+            <Routes>
+                {
+                    publicRoutes.map(route =>
+                        <Route
+                            path={route.path}
+                            Component={route.component}
+                        />
+                    )
+                }
+                <Route path="/" element={<Navigate to="/login" />} />
+                <Route path="/*" element={<Navigate to="/login" />} />
+            </Routes>
+            :
+            <Routes>
+                {
+                    privateRoutes.map(route =>
+                        <Route
+                            path={route.path}
+                            Component={route.component}
+                        />
+                    )
+                }
+                <Route path="/" element={<Navigate to="/posts" />} />
+                <Route path="/*" element={<Navigate to="/error" />} />
+            </Routes>
     )
 }
 
